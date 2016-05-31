1c52fb9dda5f744a849a732f4953e53107679976
ADMIN <ADMIN@10.20.7.250>
2016-05-31 11:44:20 -0500

    Se realizó un cifrado y descifrado estilo cesar.

--- SyFP16/src/E1/Cesar.java
+++ SyFP16/src/E1/Cesar.java
@@ -9,12 +9,40 @@ package E1;
  *
  * @author administrador1
  */
-public class Cesar {
-    
-    String m = "m";
-        for (int=0; i>m.length; i++)
+public class Cesar 
+{
+    public String cifrar(String mensaje, int constante){
+        String resultado = "";
+        
+        for(int i=0;i<mensaje.length();i++)
         {
-        int i;
-            m[i]
+            resultado = resultado + (char)(mensaje.charAt(i)+(constante));
+            
         }
+        return resultado;
     }
+    
+    public String descifrar(String mensaje, int constante1){
+        String resultado = "";
+        
+        for(int i= 0;i<mensaje.length();i++)
+        {
+            resultado = resultado + (char)(mensaje.charAt(i)-constante1);
+            //resultado = resultado + (char)(s-constante));
+        }
+        return resultado;
+    }
+    public static void main(String[] args)
+    {
+        Cesar c1 = new Cesar();
+        String s = c1.cifrar("hola", 5);
+        System.out.println(s);
+        c1.descifrar(s, 5);
+        String i= c1.descifrar(s,5);
+        System.out.println(i);
+    }
+   
+    
+    
+    
+}
