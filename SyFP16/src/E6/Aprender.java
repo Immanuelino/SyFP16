a31a77521588232c8e4b2cb511e4ff8390ca3595
immanuel <immanuel@192.168.0.138>
2016-06-09 11:00:57 -0500

    Se creo un convertidor de medidas internacionales, y una interfaz que permitito hacerlos

---
+++ SyFP16/src/E6/Aprender.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package E6;
+
+/**
+ *
+ * @author immanuel
+ */
+public class Aprender {
+    
+    
+    
+    public static void main(String[] args) {
+    Double d = new Double(3.1416); //Instanciar una variable, permite empaquetar un doble valor primitivo
+    String t = d.toString();//devuelve un string
+    int tt = d.intValue(); //devuelve un int
+    }
+   
+}
